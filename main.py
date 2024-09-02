f485438cd845eac2b6b9b367fec8318294828c53
_npt <tate.matthaidess@gmail.com>
2024-09-01 22:58:41 -0400

    Added error handling, plan to add saving of passwords

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import aiohttp.client_exceptions
+import siegeapi.exceptions
 from aiohttp import ClientSession, TCPConnector
+from siegeapi import Auth
 import decimal
 import tkinter
 import customtkinter as tk
-from siegeapi import Auth
 import asyncio
 
-global_username = None
-global_password = None
-
 def login_window():
+
+    def on_login():
+        global global_username, global_password
+        global_username = email.get()
+        global_password = user_password.get()
+        if (asyncio.run(test_login()) == "connection"):
+            error_label.configure(text="Check Your Connection")
+            print("An error has occurred while attempting to log in. Check your login credential or internet connection.")
+        elif (asyncio.run(test_login()) == True):
+            error_label.configure(text="Check User/Password")
+        else:
+            login.destroy()
 
     login = tk.CTkToplevel()
     login.title("Login")
@@ -25,93 +36,112 @@ def login_window():
     password_label.pack()
 
     user_password = tkinter.StringVar()
-    password_prompt = tk.CTkEntry(login, textvariable=user_password)
+    password_prompt = tk.CTkEntry(login, show="*", textvariable=user_password)
     password_prompt.pack(padx=5)
 
-    def on_login():
-
-        global global_username, global_password
-        global_username = email.get()
-        global_password = user_password.get()
-        login.destroy()
+    error_label = tk.CTkLabel(login, text="")
+    error_label.pack()
 
     enter_button = tk.CTkButton(login, text="Enter", command=lambda : on_login())
     enter_button.pack(pady=10)
 
-async def track_player(*args):# tracks a player
+async def test_login():
+    connector = TCPConnector(ssl=False)
+    async with ClientSession(connector=connector) as session:
+        auth = Auth(global_username, global_password, session=session)
+        try:
+            player = await auth.get_player(name="xyz swxngnn", platform="xbl")
+            print(f"{player.name}")
+        except siegeapi.exceptions.FailedToConnect:
+            login_error = True
+            return login_error
+        except aiohttp.client_exceptions.ClientConnectorError:
+            login_error = "connection"
+            return login_error
+        await auth.close()
+
+async def track_player():# tracks a player
     connector = TCPConnector(ssl=False)
     async with ClientSession(connector=connector) as session:
 
-        auth = Auth(global_username, global_password, session=session)
-        player = await auth.get_player(name=in_game_name.get(), platform=platform.get())
+        try:
+            auth = Auth(global_username, global_password, session=session)
+            player = await auth.get_player(name=in_game_name.get(), platform=platform.get())
+        except TypeError:
+            print("missing value")
+        except siegeapi.exceptions.InvalidRequest:
+            print("No results found")
+            error_label.configure(text="No Resuts found")
+        except NameError:
+            print("please login to your ubisoft account")
+            error_label.configure(text="Please login to ubisoft")
+        else:
+            print(f"Name: {player.name}")
+            print(f"Profile pic URL: {player.profile_pic_url}")
 
-        print(f"Name: {player.name}")
-        print(f"Profile pic URL: {player.profile_pic_url}")
+            await asyncio.sleep(1)
+            await player.load_playtime()
+            print(f"Total Time Played: {player.total_time_played:,} seconds")
+            print(f"Level: {player.level}")
 
-        await player.load_playtime()
-        print(f"Total Time Played: {player.total_time_played:,} seconds")
-        print(f"Level: {player.level}")
+            await asyncio.sleep(1)
+            await player.load_ranked_v2()
+            print(f"Ranked Points: {player.ranked_profile.rank_points}")
+            print(f"Rank: {player.ranked_profile.rank}")
+            print(f"Max Rank Points: {player.ranked_profile.max_rank_points}")
+            print(f"Max Rank: {player.ranked_profile.max_rank}")
+            print(f"Total Kills: {player.ranked_profile.kills}")
+            print(f"Total Deaths: {player.ranked_profile.deaths}")
 
-        await player.load_ranked_v2()
-        print(f"Ranked Points: {player.ranked_profile.rank_points}")
-        print(f"Rank: {player.ranked_profile.rank}")
-        print(f"Max Rank Points: {player.ranked_profile.max_rank_points}")
-        print(f"Max Rank: {player.ranked_profile.max_rank}")
-        print(f"Total Kills: {player.ranked_profile.kills}")
-        print(f"Total Deaths: {player.ranked_profile.deaths}")
+            def find_games_played():
+                wins = int(player.ranked_profile.wins)
+                losses = int(player.ranked_profile.losses)
+                games_played = int(wins + losses)
+                print(games_played)
 
+            find_games_played()
 
-        await player.load_progress()
-        print(f"XP: {player.xp:,}")
-        print(f"Total XP: {player.total_xp:,}")
-        print(f"XP to level up: {player.xp_to_level_up:,}")
+            kills = int(player.ranked_profile.kills)
+            deaths = int(player.ranked_profile.deaths)
+            with decimal.localcontext() as ctx:
+                ctx.prec = 3
+                ratio = decimal.Decimal(kills) / decimal.Decimal(deaths)
+            print(ratio)
 
-        kills = int(player.ranked_profile.kills)
-        deaths = int(player.ranked_profile.deaths)
-        with decimal.localcontext() as ctx:
-            ctx.prec = 3
-            ratio = decimal.Decimal(kills) / decimal.Decimal(deaths)
-        print(ratio)
+            results = tk.CTkToplevel()  # Creates a new window
+            results.title(f"{player.name}")
+            results.geometry("300x290")
 
-        await auth.close()
+            player_name = tk.CTkLabel(results, text=f"Name: {player.name}")
+            player_name.pack()
 
-    results = tk.CTkToplevel() # Creates a new window
-    results.title(f"{player.name}")
-    results.geometry("300x290")
+            player_level = tk.CTkLabel(results, text=f"Level: {player.level}")
+            player_level.pack()
 
-    frame = tk.CTkFrame(results)
+            time_played = tk.CTkLabel(results, text=(f"Total Time Played: {player.total_time_played:,} seconds"))
+            time_played.pack()
 
+            ranked_points = tk.CTkLabel(results, text=f"Ranked Points: {player.ranked_profile.rank_points}")
+            ranked_points.pack()
 
-    player_name = tk.CTkLabel(results, text=f"Name: {player.name}")
-    player_name.pack()
+            player_rank = tk.CTkLabel(results, text=f"Rank: {player.ranked_profile.rank}")
+            player_rank.pack()
 
-    player_level = tk.CTkLabel(results, text=f"Level: {player.level}")
-    player_level.pack()
+            max_ranked_points = tk.CTkLabel(results, text=f"Max Rank Points: {player.ranked_profile.max_rank_points}")
+            max_ranked_points.pack()
 
-    time_played = tk.CTkLabel(results, text=(f"Total Time Played: {player.total_time_played:,} seconds"))
-    time_played.pack()
+            max_rank = tk.CTkLabel(results, text=f"Max Rank: {player.ranked_profile.max_rank}")
+            max_rank.pack()
 
-    ranked_points = tk.CTkLabel(results, text=f"Ranked Points: {player.ranked_profile.rank_points}")
-    ranked_points.pack()
+            season_kills = tk.CTkLabel(results, text=f"Season Kills: {player.ranked_profile.kills}")
+            season_kills.pack()
 
-    player_rank = tk.CTkLabel(results, text=f"Rank: {player.ranked_profile.rank}")
-    player_rank.pack()
+            season_deaths = tk.CTkLabel(results, text=f"Season Deaths: {player.ranked_profile.deaths}")
+            season_deaths.pack()
 
-    max_ranked_points = tk.CTkLabel(results, text=f"Max Rank Points: {player.ranked_profile.max_rank_points}")
-    max_ranked_points.pack()
-
-    max_rank = tk.CTkLabel(results, text=f"Max Rank: {player.ranked_profile.max_rank}")
-    max_rank.pack()
-
-    season_kills = tk.CTkLabel(results, text=f"Season Kills: {player.ranked_profile.kills}")
-    season_kills.pack()
-
-    season_deaths = tk.CTkLabel(results, text=f"Season Deaths: {player.ranked_profile.deaths}")
-    season_deaths.pack()
-
-    kd_ratio = tk.CTkLabel(results, text="K/D Ratio: " + str(ratio))
-    kd_ratio.pack()
-
+            kd_ratio = tk.CTkLabel(results, text="K/D Ratio: " + str(ratio))
+            kd_ratio.pack()
+            await auth.close()
 
 root = tk.CTk()
 root.title("Tracker")
@@ -143,5 +173,8 @@ track_button.pack(padx=10, pady=10)
 
 login_button = tk.CTkButton(header_frame, text="Login", command=lambda : login_window())
 login_button.pack(padx=10, pady=10)
+
+error_label = tk.CTkLabel(header_frame, text="")
+error_label.pack()
 
 root.mainloop()
