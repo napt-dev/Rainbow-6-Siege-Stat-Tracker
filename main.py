1f468b3969f51ae84979a261dd8964d21d9003f1
_npt <tate.matthaidess@gmail.com>
2024-09-11 21:54:38 -0400

    made code more modular

--- main.py
+++ main.py
@@ -1,22 +1,105 @@
+import asyncio
+import decimal
+import tkinter
+
 import aiohttp.client_exceptions
+import customtkinter as tk
 import siegeapi.exceptions
 from aiohttp import ClientSession, TCPConnector
 from siegeapi import Auth
-import decimal
-import tkinter
-import customtkinter as tk
-import asyncio
 
-def login_window():
 
-    def on_login():
+def main() -> None:
+    global in_game_name, platform, ubi, error_label
+    root = tk.CTk()
+    root.title("Tracker")
+
+    header_frame = tk.CTkFrame(root)
+    header_frame.pack()
+
+    checkbox_frame = tk.CTkFrame(root)
+    checkbox_frame.pack()
+
+    in_game_name = tkinter.StringVar()
+    username_entry = tk.CTkEntry(header_frame, textvariable=in_game_name)
+    username_entry.pack(padx=10, pady=10)
+
+    platform = tk.StringVar(value="N/A")
+
+    ubi = tkinter.IntVar()
+    ubi_radio_button = tk.CTkRadioButton(checkbox_frame, text="ubi", value="uplay", variable=platform)
+    ubi_radio_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+    xbl_radio_button = tk.CTkRadioButton(checkbox_frame, text="XBOX", value="xbl", variable=platform)
+    xbl_radio_button.pack(side=tk.LEFT)
+
+    psn_radio_button = tk.CTkRadioButton(checkbox_frame, text="PSN", value="psn", variable=platform)
+    psn_radio_button.pack(side=tk.LEFT)
+
+    track_button = tk.CTkButton(header_frame, text="Track", command=lambda: asyncio.run(track_player()))
+    track_button.pack(padx=10, pady=10)
+
+    login_button = tk.CTkButton(header_frame, text="Login", command=lambda: login_window())
+    login_button.pack(padx=10, pady=10)
+
+    error_label = tk.CTkLabel(header_frame, text="")
+    error_label.pack()
+
+    root.mainloop()
+
+
+def result_window( player, ratio) -> None:
+    results = tk.CTkToplevel()  # Creates a new window
+    results.title(f"{player.name}")
+    results.geometry()
+
+    title = tk.CTkLabel(results, text=f"{player.name}")
+    title.pack()
+
+    frame = tk.CTkFrame(master=results)
+    frame.pack(padx=20, pady=20)
+
+    player_name = tk.CTkLabel(frame, text=f"{player.name}")
+    player_name.pack()
+
+    player_level = tk.CTkLabel(frame, text=f"Level: {player.level}")
+    player_level.pack()
+
+    time_played = tk.CTkLabel(frame, text=(f"Total Time Played: {player.total_time_played:,} seconds"))
+    time_played.pack()
+
+    ranked_points = tk.CTkLabel(frame, text=f"Ranked Points: {player.ranked_profile.rank_points}")
+    ranked_points.pack()
+
+    player_rank = tk.CTkLabel(frame, text=f"Rank: {player.ranked_profile.rank}")
+    player_rank.pack()
+
+    max_ranked_points = tk.CTkLabel(frame, text=f"Max Rank Points: {player.ranked_profile.max_rank_points}")
+    max_ranked_points.pack()
+
+    max_rank = tk.CTkLabel(frame, text=f"Max Rank: {player.ranked_profile.max_rank}")
+    max_rank.pack()
+
+    season_kills = tk.CTkLabel(frame, text=f"Season Kills: {player.ranked_profile.kills}")
+    season_kills.pack()
+
+    season_deaths = tk.CTkLabel(frame, text=f"Season Deaths: {player.ranked_profile.deaths}")
+    season_deaths.pack()
+
+    kd_ratio = tk.CTkLabel(frame, text="K/D Ratio: " + str(ratio))
+    kd_ratio.pack()
+
+
+def login_window() -> None:
+    def on_login() -> None:
         global global_username, global_password
         global_username = email.get()
         global_password = user_password.get()
-        if (asyncio.run(test_login()) == "connection"):
+        if (asyncio.run(test_login()) == "Connection Error"):
             error_label.configure(text="Check Your Connection")
-            print("An error has occurred while attempting to log in. Check your login credential or internet connection.")
-        elif (asyncio.run(test_login()) == True):
+            print(
+                "An error has occurred while attempting to log in. Check your login credential or internet connection.")
+        elif (asyncio.run(test_login()) == "Invalid Credintials"):
             error_label.configure(text="Check User/Password")
         else:
             login.destroy()
@@ -42,10 +125,11 @@ def login_window():
     error_label = tk.CTkLabel(login, text="")
     error_label.pack()
 
-    enter_button = tk.CTkButton(login, text="Enter", command=lambda : on_login())
+    enter_button = tk.CTkButton(login, text="Enter", command=lambda: on_login())
     enter_button.pack(pady=10)
 
-async def test_login():
+
+async def test_login() -> str:
     connector = TCPConnector(ssl=False)
     async with ClientSession(connector=connector) as session:
         auth = Auth(global_username, global_password, session=session)
@@ -53,14 +137,15 @@ async def test_login():
             player = await auth.get_player(name="xyz swxngnn", platform="xbl")
             print(f"{player.name}")
         except siegeapi.exceptions.FailedToConnect:
-            login_error = True
+            login_error = "Invalid Credintials"
             return login_error
         except aiohttp.client_exceptions.ClientConnectorError:
-            login_error = "connection"
+            login_error = "Connection Error"
             return login_error
         await auth.close()
 
-async def track_player():# tracks a player
+
+async def track_player() -> None:  # tracks a player
     connector = TCPConnector(ssl=False)
     async with ClientSession(connector=connector) as session:
 
@@ -101,80 +186,20 @@ async def track_player():# tracks a player
 
             find_games_played()
 
-            kills = int(player.ranked_profile.kills)
-            deaths = int(player.ranked_profile.deaths)
-            with decimal.localcontext() as ctx:
-                ctx.prec = 3
-                ratio = decimal.Decimal(kills) / decimal.Decimal(deaths)
-            print(ratio)
+            try:
+                kills = int(player.ranked_profile.kills)
+                deaths = int(player.ranked_profile.deaths)
+                with decimal.localcontext() as ctx:
+                    ctx.prec = 3
+                    ratio = decimal.Decimal(kills) / decimal.Decimal(deaths)
+                print(ratio)
+            except decimal.InvalidOperation:
+                ratio = "0"
 
-            results = tk.CTkToplevel()  # Creates a new window
-            results.title(f"{player.name}")
-            results.geometry("300x290")
 
-            player_name = tk.CTkLabel(results, text=f"Name: {player.name}")
-            player_name.pack()
-
-            player_level = tk.CTkLabel(results, text=f"Level: {player.level}")
-            player_level.pack()
-
-            time_played = tk.CTkLabel(results, text=(f"Total Time Played: {player.total_time_played:,} seconds"))
-            time_played.pack()
-
-            ranked_points = tk.CTkLabel(results, text=f"Ranked Points: {player.ranked_profile.rank_points}")
-            ranked_points.pack()
-
-            player_rank = tk.CTkLabel(results, text=f"Rank: {player.ranked_profile.rank}")
-            player_rank.pack()
-
-            max_ranked_points = tk.CTkLabel(results, text=f"Max Rank Points: {player.ranked_profile.max_rank_points}")
-            max_ranked_points.pack()
-
-            max_rank = tk.CTkLabel(results, text=f"Max Rank: {player.ranked_profile.max_rank}")
-            max_rank.pack()
-
-            season_kills = tk.CTkLabel(results, text=f"Season Kills: {player.ranked_profile.kills}")
-            season_kills.pack()
-
-            season_deaths = tk.CTkLabel(results, text=f"Season Deaths: {player.ranked_profile.deaths}")
-            season_deaths.pack()
-
-            kd_ratio = tk.CTkLabel(results, text="K/D Ratio: " + str(ratio))
-            kd_ratio.pack()
+            result_window(player, ratio)
             await auth.close()
 
-root = tk.CTk()
-root.title("Tracker")
 
-header_frame = tk.CTkFrame(root)
-header_frame.pack()
-
-checkbox_frame = tk.CTkFrame(root)
-checkbox_frame.pack()
-
-in_game_name = tkinter.StringVar()
-username_entry = tk.CTkEntry(header_frame, textvariable = in_game_name)
-username_entry.pack(padx=10, pady=10)
-
-platform = tk.StringVar(value="N/A")
-
-ubi = tkinter.IntVar()
-ubi_radio_button = tk.CTkRadioButton(checkbox_frame, text="ubi", value="uplay", variable=platform)
-ubi_radio_button.pack(side=tk.LEFT, padx=10, pady=10)
-
-xbl_radio_button = tk.CTkRadioButton(checkbox_frame, text="XBOX", value="xbl", variable=platform)
-xbl_radio_button.pack(side=tk.LEFT)
-
-psn_radio_button = tk.CTkRadioButton(checkbox_frame, text="PSN", value="psn", variable=platform)
-psn_radio_button.pack(side=tk.LEFT)
-
-track_button = tk.CTkButton(header_frame, text="Track", command=lambda : asyncio.run(track_player()))
-track_button.pack(padx=10, pady=10)
-
-login_button = tk.CTkButton(header_frame, text="Login", command=lambda : login_window())
-login_button.pack(padx=10, pady=10)
-
-error_label = tk.CTkLabel(header_frame, text="")
-error_label.pack()
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
